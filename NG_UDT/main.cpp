f8a591de0679f157c03552044731a343eb40ab45
christopher <christopher@christopher-vm>
2014-10-23 15:08:46 -0500

    Initial commit with all properties configured.

---
+++ NG_UDT/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: christopher
+ *
+ * Created on October 23, 2014, 3:05 PM
+ */
+
+#include <cstdlib>
+#include <udt.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
